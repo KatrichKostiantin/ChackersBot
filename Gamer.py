c92b10a02df9ae9ce64fd84f97b5705f93c6f358
Konstantin <59829955+KatrichKostiantin@users.noreply.github.com>
2020-12-06 13:04:16 +0200

    Update config and logging

--- Gamer.py
+++ Gamer.py
@@ -13,11 +13,11 @@ game = Game()
 
 class Gamer:
     def __init__(self, connection_config, loop):
-        self._api_url = f"http://{connection_config['ip']}:{connection_config['port']}"
+        self._config = connection_config
+        self._api_url = f"http://{self._config['ip']}:{self._config['port']}"
         self._session = aiohttp.ClientSession()
         self._game = game
         self._loop = loop
-        logging.basicConfig(level=logging.INFO)
 
     async def _prepare_player(self, team_name):
         logging.info(f'Prepare bot name:{team_name}')
@@ -31,6 +31,7 @@ class Gamer:
             logging.info(f"The bot was prepared. Bot color: {self._color}")
 
     async def _make_move(self, move):
+        logging.info(f"Send own move ({move}) to server")
         json = {'move': move}
         headers = {'Authorization': f'Token {self._token}'}
         async with self._session.post(
@@ -39,17 +40,22 @@ class Gamer:
                 headers=headers
         ) as resp:
             resp = (await resp.json())['data']
-            logging.info(f'Response : {resp}')
+            logging.debug(f'Response from server: {resp}')
 
     async def _get_progress_game(self):
         async with self._session.get(f'{self._api_url}/game') as resp:
+            logging.debug(f'Response : {resp}')
             return (await resp.json())['data']
 
     def start(self):
         asyncio.run_coroutine_threadsafe(self.start_playing(), self._loop)
 
     async def start_playing(self):
-        await self._prepare_player('BOT NAME')
+        logging.basicConfig(level=logging.getLevelName(self._config['logging']),
+                            format='%(asctime)s %(levelname)s %(message)s',
+                            datefmt='%H:%M:%S')
+        logging.info(f"Start bot {self._config['name']}")
+        await self._prepare_player(self._config['name'])
         while True:
             current_game_progress = await self._get_progress_game()
             is_finished = current_game_progress['is_finished']
@@ -61,7 +67,7 @@ class Gamer:
             if current_game_progress['whose_turn'] == self._color:
                 await self.move(current_game_progress)
             elif is_started:
-                await asyncio.sleep(0.10)
+                await asyncio.sleep(float(self._config['timeout']))
 
         await self._session.close()
 
@@ -72,15 +78,17 @@ class Gamer:
         self.read_opponent_move(current_game_progress)
 
         move = random.choice(self._game.get_possible_moves())
-
-
         await asyncio.sleep(0.50)
+
+        logging.debug(f"Add move ({move}) to own game")
         self._game.move(move)
         await self._make_move(move)
+        logging.info("")
 
     def read_opponent_move(self, current_game_progress):
         opponent_move = current_game_progress['last_move']
         if opponent_move is not None:
+            logging.info(f"Read opponent move :{opponent_move}")
             if opponent_move['player'] != self._color:
                 for move in opponent_move['last_moves']:
                     self._game.move(move)
